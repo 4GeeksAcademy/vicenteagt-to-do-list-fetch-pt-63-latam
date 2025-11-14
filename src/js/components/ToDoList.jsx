9b35371a44ff31bbfe899e00ea8b894beb298a09
VicenteAGT <155429830+VicenteAGT@users.noreply.github.com>
2025-11-14 23:32:06 +0000

    [Updates friday by vicenteagt]

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -1,34 +1,50 @@
 import React, { useState } from "react";
 
 export const ToDoList = () => {
-    let [task, setTask] = useState("");
+    let [task, setTask] = useState("");   //*String del input cambio de estado
 
-    function addNewTask (event){
+    function addNewTask(event) {          //*recuperacion de string
         setTask(event.target.value)
-    
+
     }
 
-    let [list, setList] = useState("")
+    let [list, setList] = useState([]) //* array cambio de estado
 
-    function addNewArrayElementToList () {    
-        setList({task})
+
+    function addNewArrayElementToList() {       //* agregar task al array
+        setList([...list, task])
+
     }
 
-    console.log(addNewArrayElementToList);
+
 
     return (
         <div>
-            <form>
-                <div className="row mt-4">
-                    <div className="col-6 m-auto">
-                        <input type="text" className="form-control" value={task} onChange={addNewTask}/>
-                    </div>
-                </div>
-            </form>
+
             <div className="row mt-4">
-                    <div className="col-6 m-auto">
-            <h1>{list}</h1>
+                <div className="col-6 m-auto">
+                    <input type="text" className="form-control" value={task} onChange={addNewTask}
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                addNewArrayElementToList();
+                            }
+                        }}
+
+                    />
+                </div>
             </div>
+
+            <div className="row mt-4">
+                <div className="col-6 m-auto">
+                    <ul>
+                        {list.map((item, index) => (
+                            <li key={index}> {item} </li>
+
+                        ))}
+                    </ul>
+                    <p>{list.length + " task"}</p>
+
+                </div>
             </div>
         </div>
 
